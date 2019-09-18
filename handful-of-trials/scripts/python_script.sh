81a5f0b3ebf493a83e494c6fca3743937109cd46
timothee <timothee.anne@inria.fr>
2019-09-18 13:43:14 +0200

    logdir path

--- handful-of-trials/scripts/python_script.sh
+++ handful-of-trials/scripts/python_script.sh
@@ -4,5 +4,5 @@ export PATH=/home/tanne/miniconda3/bin:$PATH
 . /home/tanne/miniconda3/etc/profile.d/conda.sh
 conda activate $1
 shift
-shift 
-exec python $BIN $@
+shift
+exec python $BIN $@ -logdir /Document/Prelab_LARSEN/handful-of-trials/scripts/log/
