969a39a65a27a294179a9bfb5c3132b73744f639
timothee <timothee.anne@inria.fr>
2019-09-19 16:19:11 +0200

    multi jobs with python shell code

---
+++ fast_adaptation_embedding/python_oarcall.py
@@ -0,0 +1,67 @@
+from subprocess import call
+
+import os
+import datetime
+import argparse
+import json
+
+config_file = None
+parser = argparse.ArgumentParser()
+parser.add_argument("--json", 
+                    help="""Path to json config file.
+                     Format: [{"exp":"path/to/experiment.py", 
+                     "prefix": "Prefix for result folder",
+                     "result_dir":"path/to/result/dir", 
+                     "walltime":"exp wall time",
+                     "args": " --args1 --args 2 etc", 
+                     "replicates": 3}, ...]""",
+                    type=str,
+                    required=True)
+
+arguments = parser.parse_args()
+if arguments.json is not None: config_file = arguments.json
+
+with open(config_file, 'r') as f:
+    config_dict_list = json.load(f)
+
+
+for d in config_dict_list:
+    replicates = int(d["replicates"])
+    for rep_count in range(replicates):
+        exp_folder = d["prefix"]
+        name = d["prefix"] + "_replicate_" + str(rep_count)
+        exp = d["exp"]
+        args = d["args"]
+        result_dir = d["result_dir"]
+        walltime = d["walltime"]
+        
+        dir = result_dir + "/" + exp_folder + "/" + name
+        script = dir + "/" + name + ".job"
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        f = open(script, "w")  
+
+        text = "#!/bin/bash"
+        text += 'cd '+ dir
+        text += '\n'
+        text += 'export PATH=/home/tanne/miniconda3/bin:$PATH'
+        text += '\n'
+        text += '. /home/tanne/miniconda3/etc/profile.d/conda.sh'
+        text += '\n'
+        text += 'conda activate graffiti'
+        text += '\n'
+        command = 'exec python -u ' + exp + " " + args + ' -logdir ' + dir
+        
+        print("Name: ", name)
+        print("Result dir: ", result_dir)
+        print("Command: ", command)
+        print("") 
+        
+        text += command    
+        f.write(text)
+        f.close()
+
+        run_command = "sh " + script
+        consoleio = ' -O \"' + dir + '/%jobid%.stdout\" -E \"' + dir + '/%jobid%.stderr\"'
+        print (consoleio)
+        call('oarsub -n ' + name + consoleio + ' -q production -p \"cluster=\'graffiti\'\" -l gpu=1' + ',walltime=' + walltime + ' \"' + run_command + '\"', shell=True)
