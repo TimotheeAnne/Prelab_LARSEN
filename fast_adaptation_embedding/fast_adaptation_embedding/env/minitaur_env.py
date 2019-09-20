2e111394367898c7171f55ae09e6d967de49b0c8
timothee <timothee.anne@inria.fr>
2019-09-20 15:10:34 +0200

    debug

--- fast_adaptation_embedding/fast_adaptation_embedding/env/minitaur_env.py
+++ fast_adaptation_embedding/fast_adaptation_embedding/env/minitaur_env.py
@@ -869,7 +869,7 @@ class MinitaurBulletEnv(gym.Env):
                   self._drift_weight * drift_reward + self._shake_weight * shake_reward +
                   self._survival_weight * survival_reward)
         self._objectives.append([forward_reward, energy_reward, drift_reward, shake_reward])
-        print("banana", reward)
+        print(drift_reward, self._drift_weight)
         return reward
 
     def get_objectives(self):
