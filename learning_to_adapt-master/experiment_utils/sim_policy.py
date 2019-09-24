39063af0388a897935079eac4108ae05217f29f7
timothee <timothee.anne@inria.fr>
2019-09-24 11:20:23 +0200

    sim_policy

--- learning_to_adapt-master/experiment_utils/sim_policy.py
+++ learning_to_adapt-master/experiment_utils/sim_policy.py
@@ -1,3 +1,8 @@
+import os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0, parentdir)
+
 import joblib
 import tensorflow as tf
 import argparse
