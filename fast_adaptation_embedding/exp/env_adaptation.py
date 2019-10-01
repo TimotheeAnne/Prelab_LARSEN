435660743dfbdeb80cfadfbe7b55a7c30bf95db9
timothee <timothee.anne@inria.fr>
2019-10-01 15:55:14 +0200

    test modular env adaptation

--- fast_adaptation_embedding/exp/env_adaptation.py
+++ fast_adaptation_embedding/exp/env_adaptation.py
@@ -20,89 +20,64 @@ from time import time, localtime, strftime
 import pprint
 from scipy.io import savemat
 import argparse
-from fast_adaptation_embedding.env.kinematic_arm import Arm_env
 import cma
 
-# class Cost_meta(object):
-#     def __init__(self, model, init_state, horizon, action_dim, goal, task_likelihoods):
-#        self.__model = model
-#        self.__init_state = init_state
-#        self.__horizon = horizon
-#        self.__action_dim = action_dim
-#        self.__goal = goal
-#        self.__task_likelihoods = task_likelihoods
-#
-#     def cost_given_task(self, samples, task_id):
-#         all_costs = []
-#         for s in samples:
-#             state = self.__init_state.copy()
-#             episode_cost = 0
-#             for i in range(self.__horizon):
-#                 action = s[self.__action_dim*i : self.__action_dim*i + self.__action_dim]
-#                 x = np.concatenate((state, action), axis=1).reshape(1, self.__action_dim)
-#                 diff_state = self.__model.predict(x, np.array([[task_id]])).data.cpu().numpy().flatten()
-#                 state += diff_state
-#                 episode_cost += np.linalg.norm(self.__goal - state)
-#             all_costs.append(episode_cost * self.__task_likelihoods[task_id])
-#         return np.array(all_costs)
-#
-#     def cost_fn(self, samples):
-#         all_costs = np.zeros(len(samples))
-#         for i in range(len(self.__task_likelihoods)):
-#             all_costs += self.cost_given_task(samples, i)
-#         return all_costs / np.sum(self.__task_likelihoods)
-
-
 class Cost_ensemble(object):
-    def __init__(self, ensemble_model, init_state, horizon, action_dim, goal):
+    def __init__(self, ensemble_model, init_state, horizon, action_dim, goal, pred_high, pred_low, config):
         self.__ensemble_model = ensemble_model
         self.__init_state = init_state
         self.__horizon = horizon
         self.__action_dim = action_dim
-        self.__goal = torch.FloatTensor(goal).cuda() if self.__ensemble_model.CUDA else torch.FloatTensor(goal)
+        self.__goal = goal
         self.__models = self.__ensemble_model.get_models()
+        self.__pred_high = pred_high
+        self.__pred_low = pred_low
+        self.__obs_dim = len(init_state)
+        self.__energy_weight = config['energy_weight']
+        self.__distance_weight = config['distance_weight']
+        self.__survival_weight = config['survival_weight']
+        self.__drift_weight = config['drift_weight']
+        self.__shake_weight = config['shake_weight']
+        self.__action_weight = config['action_weight']
+        self.__discount = config['discount']
+        self.__pop_batch = config['pop_batch']
 
     def cost_fn(self, samples):
         action_samples = torch.FloatTensor(samples).cuda() if self.__ensemble_model.CUDA else torch.FloatTensor(samples)
-        init_states = torch.FloatTensor(np.repeat([self.__init_state], len(samples),
-                                                  axis=0)).cuda() if self.__ensemble_model.CUDA else torch.FloatTensor(
-            np.repeat([self.__init_state], len(samples), axis=0))
-        all_costs = torch.FloatTensor(
-            np.zeros(len(samples))).cuda() if self.__ensemble_model.CUDA else torch.FloatTensor(np.zeros(len(samples)))
+        init_states = torch.FloatTensor(np.repeat([self.__init_state], len(samples), axis=0)).cuda() if  self.__ensemble_model.CUDA else torch.FloatTensor(np.repeat([self.__init_state], len(samples), axis=0))
+        all_costs = torch.FloatTensor(np.zeros(len(samples))).cuda() if self.__ensemble_model.CUDA else torch.FloatTensor(np.zeros(len(samples)))
 
         n_model = len(self.__models)
-        n_batch = min(n_model, len(samples))
-        per_batch = len(samples) / n_batch
+        n_batch = max(1, int(len(samples)/self.__pop_batch))
+        per_batch = len(samples)/n_batch
 
         for i in range(n_batch):
-            start_index = int(i * per_batch)
-            end_index = len(samples) if i == n_batch - 1 else int(i * per_batch + per_batch)
+            start_index = int(i*per_batch)
+            end_index = len(samples) if i == n_batch-1 else int(i*per_batch + per_batch)
             action_batch = action_samples[start_index:end_index]
             start_states = init_states[start_index:end_index]
-            __cost = 0
             dyn_model = self.__models[np.random.randint(0, len(self.__models))]
             for h in range(self.__horizon):
-                actions = action_batch[:, h * self.__action_dim: h * self.__action_dim + self.__action_dim]
+                actions = action_batch[:, h*self.__action_dim: h*self.__action_dim + self.__action_dim]
                 model_input = torch.cat((start_states, actions), dim=1)
                 diff_state = dyn_model.predict_tensor(model_input)
                 start_states += diff_state
-                all_costs[start_index: end_index] += torch.sum(torch.pow(start_states[:, 5::] - self.__goal, 2), dim=1)
-                # * config["discount"]**h #+ torch.sum(actions * actions, dim=1) * 0.1 * config["discount"]**h
+                for dim in range(self.__obs_dim):
+                    start_states[:, dim].clamp_(self.__pred_low[dim], self.__pred_high[dim])
 
+                action_cost = torch.sum(actions * actions, dim=1) * self.__action_weight
+                energy_cost = abs(torch.sum(start_states[:, 16:24] * start_states[:, 8:16], dim=1)) * 0.02 * self.__energy_weight
+                x_vel_cost = -diff_state[:, 28] * self.__distance_weight
+                y_vel_cost = abs(diff_state[:, 29]) * self.__drift_weight
+                shake_cost = abs(diff_state[:, 30]) * self.__shake_weight
+                survival_cost = (start_states[:, 30] < 0.13).type(start_states.dtype) * self.__survival_weight
+                all_costs[start_index: end_index] += x_vel_cost * self.__discount**h +\
+                                                     action_cost * self.__discount**h + \
+                                                     survival_cost * self.__discount**h + \
+                                                     y_vel_cost * self.__discount**h + \
+                                                     shake_cost * self.__discount**h + \
+                                                     energy_cost * self.__discount**h
         return all_costs.cpu().detach().numpy()
-
-
-def train_meta(tasks_in, tasks_out, config):
-    model = nn_model.Embedding_NN(dim_in=config["dim_in"], hidden=config["hidden_layers"], dim_out=config["dim_out"], embedding_dim=config["embedding_size"], num_tasks=len(tasks_in), CUDA=config["cuda"], SEED=None, input_limit=1.0, dropout=0.0)
-    nn_model.train_meta(model, tasks_in, tasks_out, meta_iter=config["meta_iter"], inner_iter=config["inner_iter"], inner_step=config["inner_step"], meta_step=config["meta_step"], minibatch=config["meta_batch_size"])
-    return model
-
-
-def train_model(model, train_in, train_out, task_id, config):
-    cloned_model = copy.deepcopy(model)
-    nn_model.train(cloned_model, train_in, train_out, task_id=task_id, inner_iter=config["epoch"],
-                   inner_lr=config["learning_rate"], minibatch=config["training_batch_size"])
-    return cloned_model
 
 
 def train_ensemble_model(train_in, train_out, sampling_size, config, model=None):
@@ -165,9 +140,8 @@ def execute_2(env, init_state, steps, init_mean, init_var, model, config, last_a
     current_state = env.reset()
     f_rec = config['video_recording_frequency']
     recorder = None
-    if 1: #f_rec and index_iter % f_rec == (f_rec - 1):
+    if f_rec and index_iter % f_rec == (f_rec - 1):
         recorder = VideoRecorder(env, os.path.join(config['logdir'], "iter_" + str(index_iter) + ".mp4"))
-        env.render(mode='human')
     obs = [current_state]
     acs = []
     trajectory = []
@@ -182,20 +156,22 @@ def execute_2(env, init_state, steps, init_mean, init_var, model, config, last_a
     goal = None
     for i in tqdm(range(steps)):
         cost_object = Cost_ensemble(ensemble_model=model, init_state=current_state, horizon=config["horizon"],
-                                    action_dim=env.action_space.shape[0], goal=config["goal"])
+                                    action_dim=env.action_space.shape[0], goal=goal, pred_high=pred_high,
+                                    pred_low=pred_low, config=config)
         config["cost_fn"] = cost_object.cost_fn
         if config['opt'] == "RS":
             optimizer = RS_opt(config)
-            sol =  optimizer.obtain_solution()
+            sol = optimizer.obtain_solution()
         elif config['opt'] == "CEM":
             optimizer = CEM_opt(config)
             sol = optimizer.obtain_solution(sliding_mean, init_var)
         elif config['opt'] == "CMA-ES":
             xopt, es = cma.fmin2(None, np.zeros(config["sol_dim"]), 0.5,
                                  parallel_objective=lambda x: list(config["cost_fn"](x)),
-                                 options={'maxfevals': config['max_iters']*config['popsize'],
+                                 options={'maxfevals': config['max_iters'] * config['popsize'],
                                           'popsize': config['popsize']})
             sol = xopt
+        sol = optimizer.obtain_solution()
         a = sol[0:env.action_space.shape[0]]
         next_state, r = 0, 0
         if recorder is not None:
@@ -236,91 +212,20 @@ def extract_action_seq(data):
     return np.array(actions)
 
 
-def main(args, logdir):
-    config = {
-        # exp parameters:
-        "horizon": 20,  # NOTE: "sol_dim" must be adjusted
-        "iterations": 200,
-        "random_iter": 1,
-        "episode_length": 1000,
-        "init_state": None,  # Must be updated before passing config as param
-        "action_dim": 5,
-        "video_recording_frequency": 20,
-        "logdir": logdir,
-        "load_data": None,
-        "goal": [0, 0.5],
-        "motor_velocity_limit": np.inf,
-        "angle_limit": 1,
-        "K": 1,
-
-        # # Model_parameters
-        # "dim_in": 8+31,
-        # "dim_out": 31,
-        # "hidden_layers": [128, 128],
-        # "embedding_size": 5,
-        # "cuda": True,
-
-        # # Meta learning parameters
-        # "meta_iter": 5000,
-        # "meta_step": 0.1,
-        # "inner_iter": 10,
-        # "inner_step": 0.02,
-
-        # Model learning parameters
-        "epoch": 1000,
-        "learning_rate": 1e-3,
-        "minibatch_size": 32,
-
-        # Ensemble model params'log'
-        "ensemble_epoch": 5,
-        "ensemble_dim_in": 5+7,
-        "ensemble_dim_out": 7,
-        "ensemble_hidden": [100, 100],
-        "hidden_activation": "relu",
-        "ensemble_cuda": True,
-        "ensemble_seed": None,
-        "ensemble_output_limit": None,
-        "ensemble_dropout": 0.0,
-        "n_ensembles": 1,
-        "ensemble_batch_size": 64,
-        "ensemble_log_interval": 500,
-
-        # Optimizer parameters
-        "max_iters": 1,
-        "epsilon": 0.0001,
-        "opt": "CMA-ES",
-        "lb": -1.,
-        "ub": 1.,
-        "popsize": 500,
-        "pop_batch": 16384,
-        "sol_dim": 8*20,  # NOTE: Depends on Horizon
-        "num_elites": 50,
-        "cost_fn": None,
-        "alpha": 0.1,
-        "discount": 1.
-    }
-    for (key, val) in args:
-        if key in ['horizon', 'K', 'popsize', 'iterations']:
-            config[key] = int(val)
-        elif key in ['load_data', 'opt']:
-            config[key] = val
-        elif key in ['ensemble_hidden']:
-            config[key] = [int(val), int(val), int(val)]
-        else:
-            config[key] = float(val)
-    config['sol_dim'] = config['horizon'] * config['action_dim']
-    #  ************************************************
-
+def main(config):
     logdir = os.path.join(config['logdir'], strftime("%Y-%m-%d--%H:%M:%S", localtime()) + str(np.random.randint(10**5)))
     config['logdir'] = logdir
     os.makedirs(logdir)
     with open(os.path.join(config['logdir'], "config.txt"), 'w') as f:
         f.write(pprint.pformat(config))
-    mismatches = np.array([[1., 1., 1., 1., 1.]])
-    n_task = len(mismatches)
-    goal = [1000, 0]
+    n_task = 1
     render = False
-    envs = [Arm_env(goal=tuple(config["goal"]), joint_mismatch=mismatches[0]) for i in range(n_task)]
+    envs = [gym.make("MinitaurBulletEnv_fastAdapt-v0", render=render, distance_weight=config['distance_weight'],
+                     energy_weight=config['energy_weight'], survival_weight=config['survival_weight'],
+                     drift_weight=config['drift_weight'], shake_weight=config['shake_weight'],
+                     action_weight=config['action_weight'], motor_velocity_limit=config['motor_velocity_limit'],
+                     angle_limit=config['angle_limit'])
+            for i in range(n_task)]
     random_iter = config['random_iter']
     data = n_task * [None]
     models = n_task * [None]
@@ -331,7 +236,7 @@ def main(args, logdir):
     all_costs = []
 
     for i in range(n_task):
-        with open(os.path.join(config['logdir'], "ant_costs_task_"+ str(i)+".txt"), "w+") as f:
+        with open(os.path.join(config['logdir'], "ant_costs_task_" + str(i)+".txt"), "w+") as f:
             f.write("")
 
     traj_obs, traj_acs, traj_rets, traj_rews, traj_error = [], [], [], [], []
@@ -371,8 +276,6 @@ def main(args, logdir):
             all_costs.append(c)
         else:
             '''------------Update models------------'''
-            # with open("random_data.pk", 'wb') as f:
-            #     pickle.dump(data, f)
             x, y, high, low = process_data(data[env_index])
             print("Learning model...")
             models[env_index] = train_ensemble_model(train_in=x, train_out=y, sampling_size=-1, config=config, model=models[env_index])
@@ -435,5 +338,69 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--config', action='append', nargs=2, default=[])
     parser.add_argument('-logdir', type=str, default='log')
     args = parser.parse_args()
+    logdir = args.logdir
+    config = {
+        # exp parameters:
+        "horizon": 20,  # NOTE: "sol_dim" must be adjusted
+        "iterations": 200,
+        "random_iter": 100,
+        "episode_length": 1000,
+        "init_state": None,  # Must be updated before passing config as param
+        "action_dim": 8,
+        "video_recording_frequency": 20,
+        "logdir": logdir,
+        "load_data": None,
+        "distance_weight": 1.0,
+        "energy_weight": 0.,
+        "shake_weight": 0.,
+        "drift_weight": 0.,
+        "survival_weight": 0.,
+        "action_weight": 0.,
+        "motor_velocity_limit": np.inf,
+        "angle_limit": 1,
+        "K": 1,
 
-    main(args.config, args.logdir)
+        # Model learning parameters
+        "epoch": 1000,
+        "learning_rate": 1e-3,
+        "minibatch_size": 32,
+
+        # Ensemble model params'log'
+        "ensemble_epoch": 5,
+        "ensemble_dim_in": 8+31,
+        "ensemble_dim_out": 31,
+        "ensemble_hidden": [200, 200, 100],
+        "hidden_activation": "relu",
+        "ensemble_cuda": True,
+        "ensemble_seed": None,
+        "ensemble_output_limit": None,
+        "ensemble_dropout": 0.0,
+        "n_ensembles": 1,
+        "ensemble_batch_size": 64,
+        "ensemble_log_interval": 500,
+
+        # Optimizer parameters
+        "max_iters": 1,
+        "epsilon": 0.0001,
+
+        "lb": -0.5,
+        "ub": 0.5,
+        "popsize": 500,
+        "pop_batch": 16384,
+        "sol_dim": 8*20,  # NOTE: Depends on Horizon
+        "num_elites": 50,
+        "cost_fn": None,
+        "alpha": 0.1,
+        "discount": 1.
+    }
+    for (key, val) in args.config:
+        if key in ['horizon', 'K', 'popsize', 'iterations']:
+            config[key] = int(val)
+        elif key in ['load_data']:
+            config[key] = val
+        elif key in ['ensemble_hidden']:
+            config[key] = [int(val), int(val), int(val)]
+        else:
+            config[key] = float(val)
+    config['sol_dim'] = config['horizon'] * config['action_dim']
+    main(config)
